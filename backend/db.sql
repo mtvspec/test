f63b705fe889da777f59c2980165959f8c11bcb4
mtvspec <mtv.spec@gmail.com>
2015-11-09 09:30:44 +0600

    adds is_deleted column

--- backend/db.sql
+++ backend/db.sql
@@ -19,19 +19,20 @@ SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "
 -- Извлечь физическое лицо по ИИН
 SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID", is_deleted AS "isDeleted" FROM e_persons WHERE iin = {iin};
 -- Извлечь всех существующих физических лиц
-SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 0 ORDER BY iin ASC;
+SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 'N' ORDER BY iin ASC;
 -- Извлечь существующие физическое лицо по ИИН
-SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 0 AND iin = {iin};
+SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 'N' AND iin = {iin};
 -- Извлечь всех несуществующих физических лиц
-SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 1 ORDER BY iin ASC;
+SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 'Y' ORDER BY iin ASC;
 -- Извлечь несуществующее физическое лицо по ИИН
-SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 1 AND iin = {iin};
+SELECT iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", dob, gender_id AS "genderID" FROM e_persons WHERE is_deleted = 'Y' AND iin = {iin};
 -- Вставить физическое лицо
 INSERT INTO e_persons (iin, last_name, first_name, middle_name, dob, gender_id) VALUES ({iin}, {lastName}, {firstName}, {middleName}, {dob}, {genderID}) RETURNING iin;
 -- Обновить физическое лицо
 UPDATE e_persons SET iin = {iin}, last_name = {lastName}, first_name = {firstName} middle_name = {middleName}, dob = {dob}, gender_id = {genderID} WHERE iin = {iin} RETURNING iin;
 -- Удалить физическо лицо
-UPDATE e_persons SET is_deleted = 1 WHERE iin = {iin} RETURNING iin; 
+UPDATE e_persons SET is_deleted = 1 WHERE iin = {iin} RETURNING iin;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Журнал истории изменения сущности 'Физическое лицо'
 -- Created
 CREATE TABLE log.e_persons (
@@ -59,6 +60,7 @@ CREATE TABLE log.e_persons (
 SELECT id, session_id AS "sessionID", man_date AS "manDate", type_id AS "typeID", iin, last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName", gender_id AS "genderID", is_deleted AS "isDeleted" FROM log.e_persons ORDER BY id ASC;
 -- Вставить изменение в журнал истории изменения сущности 'Физическое лицо'
 INSERT INTO log.e_persons (session_id, manipulation_type_id, iin, last_name, first_name, middle_name, gender_id, is_deleted) values ({sessionID}, {manipulationTypeID}, {iin}, {lastName}, {firstName}, {middleName}, {genderID}, {isDeleted}) RETURNING id;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Сущность 'Юридические лица'
 -- Created
 CREATE TABLE e_companies (
@@ -68,26 +70,27 @@ CREATE TABLE e_companies (
   PRIMARY KEY (bin),
   UNIQUE (company_name),
   FOREIGN KEY (is_deleted) REFERENCES dict.is_deleted(id),
-  CHECK ()
+  CHECK (is_deleted IN ('N','Y'))
 );
 -- Извлечь все юридические лица
 SELECT bin, company_name AS "companyName", is_deleted AS "isDeleted" FROM e_companies ORDER BY bin ASC;
 -- Извлечь юридическое лицо по БИН
 SELECT bin, company_name AS "companyName", is_deleted AS "isDeleted" FROM e_companies WHERE bin = {bin};
 -- Извлечь все существующее юридические лица
-SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 0 ORDER BY bin ASC;
+SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 'N' ORDER BY bin ASC;
 -- Извлечь существующее юридическое лицо по БИН
-SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 0 AND bin = {bin};
+SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 'N' AND bin = {bin};
 -- Извлечь все не существующие юридические лица
-SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 1 ORDER BY bin ASC;
+SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 'Y' ORDER BY bin ASC;
 -- Извлечь не существующее юридическое лицо по БИН
-SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 1 AND bin = {bin};
+SELECT bin, company_name AS "companyName" FROM e_companies WHERE is_deleted = 'Y' AND bin = {bin};
 -- Вставить юридическое лицо
 INSERT INTO e_companies (bin, company_name) VALUES ({bin}, {companyName}) RETURNING bin;
 -- Обновить юридическое лицо
 UPDATE e_companies SET bin = {bin}, company_name = {companyName} WHERE bin = {bin} RETURNING bin;
 -- Удалить юридическое лицо
-UPDATE e_companies SET is_deleted = 1 WHERE bin = {bin} RETURNING bin;
+UPDATE e_companies SET is_deleted = 'Y' WHERE bin = {bin} RETURNING bin;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Журнал истории изменения сущности 'Юридическое лицо'
 CREATE TABLE log.e_companies (
   id SERIAL NOT NULL,
@@ -103,6 +106,7 @@ CREATE TABLE log.e_companies (
   FOREIGN KEY (bin) REFERENCES e_companies(bin),
   CHECK (is_deleted IN ('N','Y'))
 );
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Сущность 'Должность'
 CREATE TABLE e_positions (
   id SERIAL NOT NULL,
@@ -112,6 +116,29 @@ CREATE TABLE e_positions (
   UNIQUE (position_name),
   CHECK (is_deleted IN ('N','Y'))
 );
+-- Извлечь все должности
+SELECT id, position_name AS "positionName", is_deleted AS "isDeleted" FROM e_positions ORDER BY id ASC;
+-- Извлечь должность по идентификатору должности
+SELECT id, position_name AS "positionName", is_deleted AS "isDeleted" FROM e_positions WHERE id = {id};
+-- Извлечь должность по наименованию должности
+SELECT id FROM e_positions WHERE position_name = {positionName};
+-- Извлечь все существующие должности
+SELECT id, position_name AS "positionName" FROM e_positions WHERE is_deleted = 'N' ORDER BY id ASC;
+-- Извлечь существующую должность по идентификатору должности
+SELECT id, position_name AS "positionName" FROM e_positions WHERE is_deleted = 'N' AND id = {id};
+-- Извлечь все несуществующие должности
+SELECT id, position_name AS "positionName" FROM e_positions WHERE is_deleted = 'Y' ORDER BY id ASC;
+-- Извлечь несуществующую должность по идентификатору должности
+SELECT id, position_name AS "positionName" FROM e_positions WHERE is_deleted = 'Y' AND id = {id};
+-- Вставить должность
+INSERT INTO e_positions (position_name) VALUES ({positionName}) RETURNING id;
+-- Обновить должность по идентификатору должности 
+UPDATE e_positions SET position_name = {positionName} WHERE id = {id};
+-- Удалить должность по идентификатору должности
+UPDATE e_positions SET is_deleted = 'Y' WHERE id = {id};
+-- Восстановить должность по идентификатору должности
+UPDATE e_positions SET is_deleted = 'N' WHERE id = {id};
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Журнал истории изменения сущности 'Должность'
 CREATE TABLE log.e_positions (
   id SERIAL NOT NULL,
@@ -127,14 +154,17 @@ CREATE TABLE log.e_positions (
   FOREIGN KEY (position_id) REFERENCES e_positions(id),
   CHECK (is_deleted IN ('N','Y'))
 );
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Сущность 'Подразделения юридических лиц'
 CREATE TABLE e_divisions (
   id SERIAL NOT NULL,
   parent_division_id INTEGER,
   division_name VARCHAR(400) NOT NULL,
+  is_deleted CHAR(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (id),
   UNIQUE (division_name),
-  FOREIGN KEY (parent_division_id) REFERENCES e_divisions(id)
+  FOREIGN KEY (parent_division_id) REFERENCES e_divisions(id),
+  CHECK (is_deleted IN ('N', 'Y'))
 );
 -- Извлечь все подразделения юридических лиц
 SELECT id, parent_division_id AS "parentDivisionID", division_name AS "divisionName" FROM e_divisions ORDER BY id ASC;
@@ -144,6 +174,7 @@ SELECT id, parent_division_id AS "parentDivisionID", division_name AS "divisionN
 -- ==========================================================================================================================================================================================================================--
 -- Created
 CREATE SCHEMA dict;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Справочник 'Пол физического лица (Мужской/Женский)'
 -- Created
 CREATE TABLE dict.d_genders (
@@ -155,8 +186,9 @@ CREATE TABLE dict.d_genders (
 );
 -- Извлечь значения справочника 'Пол физического лица'
 SELECT id, gender_name AS "genderName" FROM dict.d_genders ORDER BY id ASC;
--- Извлечь значение справоничка по идентификатору справочного значения
+-- Извлечь значение справоничка 'Пол физического лица' по идентификатору значения справочника
 SELECT id, gender_name AS "genderName" FROM dict.d_genders WHERE id = {id};
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Справочник 'Удален? (Нет/Да)'
 -- Created
 CREATE TABLE dict.is_deleted (
@@ -166,6 +198,11 @@ CREATE TABLE dict.is_deleted (
   UNIQUE (condition_name),
   CHECK (id IN ('N', 'Y'))
 );
+-- Извлечь значения справочника 'Удален (Нет/Да)'
+SELECT id, condition_name AS "conditionName" FROM dict.is_deleted ORDER BY id ASC;
+-- Извлечь значение справочника 'Удален (Нет/Да)' по идентификатору значения справочника
+SELECT id, condition_name AS "conditionName" FROM dict.is_deleted WHERE id = {id};
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Справочник 'Тип манипуляции над данными'
 -- Created
 CREATE TABLE dict.manipulation_type (
@@ -174,15 +211,12 @@ CREATE TABLE dict.manipulation_type (
   PRIMARY KEY (id),
   UNIQUE (manipulation_type_name)
 );
--- Извлечь состояния
-SELECT id, condition_name AS "conditionName" FROM dict.is_deleted ORDER BY id ASC;
--- Извлечь состояние по идентификатору состояния
-SELECT id, condition_name AS "conditionName" FROM dict.is_deleted WHERE id = {id};
 -- ==========================================================================================================================================================================================================================--
 -- Метаданные
 -- ==========================================================================================================================================================================================================================--
 -- Created
 CREATE SCHEMA meta;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Сущность 'Сессии'
 -- Created
 CREATE TABLE meta.e_sessions (
@@ -201,6 +235,7 @@ CREATE TABLE meta.e_sessions (
 SELECT id, role_id AS "roleID", open_date AS "openDate", close_date AS "closeDate" status FROM meta.e_sessions WHERE user_id = {userID};
 -- Вставить сессию
 INSERT INTO meta.e_sessions (user_id, role_id) VALUES ({userID}, {roleID}) RETIRNING id;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Сущность 'Пользователи'
 -- Created
 CREATE TABLE meta.e_users (
@@ -230,27 +265,34 @@ UPDATE meta.e_users SET is_deleted = 'N' WHERE id = {id};
 UPDATE meta.e_users SET is_blocked = 'Y' WHERE id = {id};
 -- Разблокировать пользователя по идентификатору пользователя
 UPDATE meta.e_users SET is_blocked = 'N' WHERE id = {id};
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Сущность 'Роли'
 -- Created
 CREATE TABLE meta.e_roles (
   id SERIAL NOT NULL,
   role_name VARCHAR(200) NOT NULL,
+  is_deleted CHAR(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (id),
-  UNIQUE (role_name)
+  UNIQUE (role_name),
+  CHECK (is_deleted IN ('N', 'Y'))
 );
 -- Извлечь все роли
 SELECT id, role_name FROM meta.e_roles ORDER BY id ASC;
 -- Вставить роль
 INSERT INTO meta.e_roles (role_name) VALUES ({roleName});
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Связь 'Роли - Пользователи'
 CREATE TABLE meta.r_e_roles_e_users (
   id SERIAL NOT NULL,
   role_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
+  is_deleted CHAR(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (role_id, user_id),
   UNIQUE (id)
   FOREIGN KEY (role_id) REFERENCES meta.e_roles(id),
-  FOREIGN KEY (user_id) REFERENCES meta.e_users(id)
+  FOREIGN KEY (user_id) REFERENCES meta.e_users(id),
+  FOREIGN KEY (is_deleted) REFERENCES dict.is_deleted(id),
+  CHECK (is_deleted IN ('N', 'Y'))
 );
 -- Извлечь все связи 'Роли - Пользователи'
 SELECT id, role_id AS "roleID", user_id AS "userID" FROM meta.r_e_roles_e_users ORDER BY id ASC;
@@ -262,21 +304,27 @@ CREATE TABLE r_e_companies_e_divisions (
   id SERIAL NOT NULL,
   company_id CHAR(12) NOT NULL,
   division_id INTEGER NOT NULL,
+  is_deleted CHAR(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (company_id, division_id),
   UNIQUE (id),
   FOREIGN KEY (company_id) REFERENCES e_companies(bin),
-  FOREIGN KEY (division_id) REFERENCES e_divisions(id)
+  FOREIGN KEY (division_id) REFERENCES e_divisions(id),
+  CHECK (is_deleted IN ('N', 'Y'))
 );
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Связь 'Подразделения - Должности'
 CREATE TABLE r_e_divisions_e_positions (
   id SERIAL NOT NULL,
   division_id INTEGER NOT NULL,
   position_id INTEGER NOT NULL,
+  is_deleted CHAR(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (division_id, position_id),
   UNIQUE (id),
   FOREIGN KEY (division_id) REFERENCES e_divisions(id),
-  FOREIGN KEY (position_id) REFERENCES e_positions(id)
+  FOREIGN KEY (position_id) REFERENCES e_positions(id),
+  CHECK (is_deleted IN ('N', 'Y'))
 );
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Связь 'Должности - Физические лица'
 CREATE TABLE r_positions_e_persons (
   id SERIAL NOT NULL,
@@ -292,3 +340,4 @@ CREATE TABLE r_positions_e_persons (
   FOREIGN KEY (serssion_id) REFERENCES meta.e_sessions(id),
   CHECK (is_deleted IN ('N','Y')) 
 );
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
