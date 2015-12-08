bace940c60408d34769bbf6efd3243d960c4d5da
mtvspec <mtv.spec@gmail.com>
2015-12-08 08:27:40 +0600

    adds todo

--- frontend/public/views/login/roles/RolesCtrl.js
+++ frontend/public/views/login/roles/RolesCtrl.js
@@ -4,8 +4,41 @@
   angular.module('app')
   .controller('RolesCtrl', function ($http, $state, UserModel) {
     var vm = this,
-    User = UserModel.getUser();
-    vm.User = User;
+    _personsUrl = '/api/persons',
+    persons = [];
+    vm.User = UserModel.getUser(),
+    vm.persons = persons;
+
+    $http({
+      method: 'GET',
+      url: _personsUrl
+    }).then(function (response) {
+      if (response.status === 200) {
+        vm.persons = response.data;
+      }
+    });
+
+    vm.getPersonFirstnameByID = getPersonFirstnameByID;
+    function getPersonFirstnameByID(persons, personID) {
+      var firstName;
+      for (var i = 0; i < persons.length; i++) {
+        if (persons[i].id === personID) {
+          firstName = persons[i].firstName;
+        }
+      }
+      return firstName;
+    };
+
+    vm.getPersonMiddlenameByID = getPersonMiddlenameByID;
+    function getPersonMiddlenameByID(persons, personID) {
+      var middleName;
+      for (var i = 0; i < persons.length; i++) {
+        if (persons[i].id === personID) {
+          middleName = persons[i].middleName;
+        }
+      }
+      return middleName;
+    };
 
     vm.selectRole = selectRole;
     function selectRole(id) {
